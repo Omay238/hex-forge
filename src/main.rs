6e71482843718397acea7b9163497c77789c1ae4
Omay238 <238maculo238@gmail.com>
2025-11-23 11:36:04 -0700

    movement

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use bevy::prelude::*;
 
+const PLAYER_SPEED: f32 = 250.0;
+const CAMERA_SPEED: f32 = 4.0;
+
+#[derive(Component)]
+struct Player;
+
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
     app.add_systems(Startup, setup);
+    app.add_systems(Update, (move_player, update_camera).chain());
     app.run();
 }
 
@@ -13,6 +20,11 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>
 ) {
     commands.spawn(Camera2d);
+
+    commands.spawn((
+        Player,
+        Transform::from_xyz(0.0, 0.0, 2.0),
+    ));
 
     let mut shapes: Vec<Handle<Mesh>> = Vec::new();
 
@@ -39,4 +51,42 @@ fn setup(
             Transform::from_xyz(0.0, 0.0, 0.0)
         ));
     }
+}
+
+fn move_player(
+    mut player: Single<&mut Transform, With<Player>>,
+    time: Res<Time>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+) {
+    let mut movement = Vec2::ZERO;
+
+    if keyboard_input.pressed(KeyCode::KeyW) {
+        movement.y += 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::KeyS) {
+        movement.y -= 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::KeyA) {
+        movement.x -= 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::KeyD) {
+        movement.x += 1.0;
+    }
+
+    let move_delta = movement.normalize_or_zero() * PLAYER_SPEED * time.delta_secs();
+    player.translation += move_delta.extend(0.0);
+    player.translation = player.translation.clamp(Vec3::new(-17670.0, -20330.0, -10.0), Vec3::new(17670.0, 20330.0, 10.0))
+}
+
+fn update_camera(
+    mut camera: Single<&mut Transform, (With<Camera2d>, Without<Player>)>,
+    player: Single<&Transform, (With<Player>, Without<Camera2d>)>,
+    time: Res<Time>,
+) {
+    let Vec3 { x, y, .. } = player.translation;
+    let direction = Vec3::new(x, y, camera.translation.z);
+
+    camera
+        .translation
+        .smooth_nudge(&direction, CAMERA_SPEED, time.delta_secs());
 }
