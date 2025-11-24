fa71af8ead6234d0a5f10d7358e81fae0fd13496
Omay238 <238maculo238@gmail.com>
2025-11-23 17:41:42 -0700

    migrate to library

---
+++ src/lib.rs
@@ -0,0 +1,234 @@
+use bevy::prelude::*;
+
+use std::collections::HashMap;
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Component, Copy, Clone, PartialEq)]
+struct Hex(IVec2);
+
+impl std::fmt::Display for Hex {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0.to_string())
+    }
+}
+
+#[derive(Resource, Clone, Copy)]
+pub struct HexForgeConfig {
+    pub player_speed: f32,
+    pub camera_speed: f32,
+    pub stroke_color: Color,
+    pub fill_color: Color,
+    pub hover_color: Color,
+}
+
+#[derive(Resource, Clone)]
+struct HexForgeMemory {
+    active_hexes: HashMap<IVec2, Entity>,
+    inactive_hexes: HashMap<IVec2, Entity>,
+}
+
+pub struct HexForge {
+    pub config: HexForgeConfig,
+    memory: HexForgeMemory,
+}
+
+fn hex_pos(pos: Vec2) -> Hex {
+    let mut col = (pos.x / 186.0).round();
+    let mut row = ((pos.y - if col % 2.0 == 0.0 { 107.0 } else { 0.0 }) / 214.0).round();
+
+    if col == -0.0 {
+        col = 0.0;
+    }
+    if row == -0.0 {
+        row = 0.0;
+    }
+
+    Hex(IVec2::new(col as i32, row as i32))
+}
+
+fn spawn_hex(
+    commands: &mut Commands,
+    meshes: &mut Assets<Mesh>,
+    materials: &mut Assets<ColorMaterial>,
+    position: Vec2,
+    coord: IVec2,
+    config: &Res<HexForgeConfig>,
+) {
+    let fill_mesh = meshes.add(
+        ConvexPolygon::new(vec![
+            Vec2::new(62.0, -107.0),
+            Vec2::new(124.0, 0.0),
+            Vec2::new(62.0, 107.0),
+            Vec2::new(-62.0, 107.0),
+            Vec2::new(-124.0, 0.0),
+            Vec2::new(-62.0, -107.0),
+        ])
+        .unwrap()
+        .mesh(),
+    );
+
+    let fill_mat = materials.add(config.fill_color);
+
+    let stroke_mesh = meshes.add(Polyline2d::new(vec![
+        Vec2::new(62.0, -107.0),
+        Vec2::new(124.0, 0.0),
+        Vec2::new(62.0, 107.0),
+        Vec2::new(-62.0, 107.0),
+        Vec2::new(-124.0, 0.0),
+        Vec2::new(-62.0, -107.0),
+    ]));
+
+    let stroke_mat = materials.add(config.stroke_color);
+
+    commands
+        .spawn((
+            Transform::from_xyz(position.x, position.y, 0.0),
+            Visibility::default(),
+        ))
+        .with_children(|p| {
+            p.spawn((
+                Mesh2d(fill_mesh),
+                MeshMaterial2d(fill_mat),
+                Hex(coord),
+                Transform::from_xyz(0.0, 0.0, 0.0),
+            ));
+            p.spawn((
+                Mesh2d(stroke_mesh),
+                MeshMaterial2d(stroke_mat),
+                Transform::from_xyz(0.0, 0.0, 0.1),
+            ));
+        });
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+
+    config: Res<HexForgeConfig>,
+) {
+    commands.spawn(Camera2d);
+
+    commands.spawn((Player, Transform::from_xyz(0.0, 0.0, 2.0)));
+
+    for x in -5..5 {
+        for y in -5..5 {
+            let root_position = Vec2::new(
+                (186 * x) as f32,
+                (y * 214 + (if x % 2 == 0 { 107 } else { 0 })) as f32,
+            );
+
+            spawn_hex(
+                &mut commands,
+                &mut meshes,
+                &mut materials,
+                root_position,
+                IVec2::new(x, y),
+                &config
+            );
+        }
+    }
+}
+
+fn move_player(
+    mut player: Single<&mut Transform, With<Player>>,
+    time: Res<Time>,
+    config: Res<HexForgeConfig>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+) {
+    let mut movement = Vec2::ZERO;
+
+    if keyboard_input.pressed(KeyCode::KeyW) {
+        movement.y += 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::KeyS) {
+        movement.y -= 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::KeyA) {
+        movement.x -= 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::KeyD) {
+        movement.x += 1.0;
+    }
+
+    let move_delta = movement.normalize_or_zero() * config.player_speed * time.delta_secs();
+    player.translation += move_delta.extend(0.0);
+}
+
+fn update_camera(
+    mut camera: Single<&mut Transform, (With<Camera2d>, Without<Player>)>,
+    player: Single<&Transform, (With<Player>, Without<Camera2d>)>,
+    time: Res<Time>,
+    config: Res<HexForgeConfig>,
+) {
+    let Vec3 { x, y, .. } = player.translation;
+    let direction = Vec3::new(x, y, camera.translation.z);
+
+    camera
+        .translation
+        .smooth_nudge(&direction, config.camera_speed, time.delta_secs());
+}
+
+fn mouse_interaction(
+    camera_query: Single<(&Camera, &GlobalTransform)>,
+    window: Single<&Window>,
+
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut tiles: Query<(&Hex, &mut MeshMaterial2d<ColorMaterial>)>,
+
+    config: Res<HexForgeConfig>,
+) {
+    let (camera, camera_transform) = *camera_query;
+
+    if let Some(cursor_position) = window.cursor_position()
+        && let Ok(world_pos) = camera.viewport_to_world_2d(camera_transform, cursor_position)
+        && let Ok(viewport_check) =
+            camera.world_to_viewport(camera_transform, world_pos.extend(0.0))
+        && let Ok(world_check) = camera.viewport_to_world_2d(camera_transform, viewport_check.xy())
+    {
+        let pos = hex_pos(world_check);
+
+        for (coord, mut material_handle) in &mut tiles {
+            let mat = materials.get_mut(&material_handle.0).unwrap();
+
+            if coord == &pos {
+                mat.color = config.hover_color;
+            } else {
+                mat.color = config.fill_color;
+            }
+        }
+    }
+}
+
+impl HexForge {
+    pub fn new(config: HexForgeConfig) -> Self {
+        Self {
+            config,
+            memory: HexForgeMemory {
+                active_hexes: HashMap::new(),
+                inactive_hexes: HashMap::new(),
+            },
+        }
+    }
+
+    pub fn start(&mut self) {
+        let mut app = App::new();
+        app.add_plugins(DefaultPlugins);
+
+        app.insert_resource(self.config);
+        app.insert_resource(self.memory.clone());
+
+        app.add_systems(Startup, setup);
+        app.add_systems(
+            Update,
+            ((move_player, update_camera).chain(), mouse_interaction),
+        );
+        app.run();
+    }
+
+    pub fn save(&mut self) -> HashMap<IVec2, Entity> {
+        self.memory.inactive_hexes.clone()
+    }
+}
